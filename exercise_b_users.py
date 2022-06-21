78edc7faa7fa9ebf4ae8733f519595f03f37bd90
Nick Murray <nicholas.murray1332@googlemail.com>
2022-06-21 20:28:46 +0100

    b7 answered

--- exercise_b_users.py
+++ exercise_b_users.py
@@ -80,7 +80,7 @@ for even_num in users["Avril"]["lottery_numbers"]:
   if even_num % 2 == 0:
     print(even_num, end = " ")
 # 7. Erik is one lottery number short! Add the number `7` to be included in his lottery numbers
-
+users["Erik"]["lottery_numbers"].append(7)
 # 8. Change Erik's hometown to Edinburgh
 users["Erik"]["home_town"]="Edinburgh"
 # 9. Add a pet dog to Erik called "fluffy"
