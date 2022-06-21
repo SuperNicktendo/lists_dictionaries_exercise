b8e8af6c7b05ad39d1403aceac6ae5707ebc698f
Nick Murray <nicholas.murray1332@googlemail.com>
2022-06-21 18:23:14 +0100

    corrected brackets error

--- exercise_a_stops.py
+++ exercise_a_stops.py
@@ -1,13 +1,13 @@
 stops = [ "Croy", "Cumbernauld", "Falkirk High", "Linlithgow", "Livingston", "Haymarket" ]
 
 #1. Add "Edinburgh Waverley" to the end of the list
-stops.append["Edinburgh Waverley"]
+stops.append("Edinburgh Waverley")
 #2. Add "Glasgow Queen St" to the start of the list
-stops.insert[0, "Glasgow Queen St"]
+stops.insert(0, "Glasgow Queen St")
 #3. Add "Polmont" at the appropriate point (between "Falkirk High" and "Linlithgow")
-stops.insert[3, "Polmont"]
+stops.insert(3, "Polmont")
 #4. Print out the index position of "Linlithgow"
-print[stops.index["Linlithgow"]]
+print(stops.index("Linlithgow"))
 #5. Remove "Livingston" from the list using its name
 #6. Delete "Cumbernauld" from the list by index
 #7. Print the number of stops there are in the list
